d0fccf7e41a30e7f248526a272707e105bd7e848
KorolyovAlexander <amkorolyov@gmail.com>
2016-11-10 14:37:43 +0200

    NoteDirective was added

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 /**
  * Created by Samsung on 06.11.2016.
  */
-import MainController from 'MainController';
-import NoteDirective from 'NoteDirective';
+
+class MainController{
+
+    constructor(){
+        this.notes = [];
+    }
+
+    addNote(text){
+        this.notes.unshift(text);
+        this.text = '';
+    }
+
+    deleteNote(index){
+        this.notes.splice(index, 1);
+    }
+}
+
+class NoteDirective{
+
+    constructor(){
+        this.scope = {
+            value: '=',
+            delete: '&'
+        };
+        this.restrict = 'E';
+        this.templateUrl =  'note-directive.html';
+        this.controller = NoteController;
+        this.bindToController = true;
+        this.controllerAs = 'vm';
+    }
+}
+
+class NoteController{
+
+    onClick() {
+        this.delete();
+    };
+}
 
 var app = angular.module('App', [])
     .controller('MainController', MainController)
